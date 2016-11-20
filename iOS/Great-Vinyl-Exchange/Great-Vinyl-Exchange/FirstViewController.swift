7b3077eae20de387b575a09213790548fc186f50
Daniel Ley <danny.ley13@gmail.com>
2016-11-19 21:40:07 -0600

    Add iOS Project

---
+++ iOS/Great-Vinyl-Exchange/Great-Vinyl-Exchange/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Great-Vinyl-Exchange
+//
+//  Created by Daniel Ley on 11/19/16.
+//  Copyright © 2016 daniel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
